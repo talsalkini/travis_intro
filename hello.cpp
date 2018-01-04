87c067db40371d7c2f51147301e586670b9cc951
talsalkini <t.alsalkini@hotmail.com>
2018-01-04 13:33:01 +0000

    init commit

---
+++ hello.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    std::cout << "***************************************\n";
+    std::cout << "++++++++++++++ Hello CPP! +++++++++++++" << "\n";
+    std::cout << "***************************************\n";
+}
